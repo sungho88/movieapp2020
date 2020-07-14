36a16528c0e84997ee72c6abcf0a6c6a6964780b
SUNGHO JANG <sunghojang.dev@gmail.com>
2020-07-14 10:53:38 +0900

    Updates

---
+++ precache-manifest.75d2ec78b28bd83b713fad89b6108a4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a8d5d449e862b8f3eaaee99d0dfdbd",
+    "url": "/movieapp2020/index.html"
+  },
+  {
+    "revision": "0c931062455faa3c344b",
+    "url": "/movieapp2020/static/js/2.09011713.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieapp2020/static/js/2.09011713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "527c34f043af71eb5620",
+    "url": "/movieapp2020/static/js/main.df61f487.chunk.js"
+  },
+  {
+    "revision": "92d0a99e64891c82c30d",
+    "url": "/movieapp2020/static/js/runtime-main.ae3b6c42.js"
+  }
+]);
